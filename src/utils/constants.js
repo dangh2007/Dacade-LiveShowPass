602f40fc81d50632f1b79f497f5c913f9c63bf44
dangh2007 <76785849+dangh2007@users.noreply.github.com>
2022-08-17 12:40:47 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,3 @@
-const ERC20_DECIMALS = 18;
-const MPContractAddress = "0xbB45B2aBCF1b4C9653d482229dc65F0815353BE6";
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 
 const ERC20_DECIMALS = 18
@@ -10,4 +7,3 @@ const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 
 export {ERC20_DECIMALS, cUSDContractAddress, MPContractAddress}
-export { ERC20_DECIMALS, cUSDContractAddress, MPContractAddress };
